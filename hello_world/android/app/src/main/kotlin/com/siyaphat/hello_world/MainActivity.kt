9a365d86813a28f7359ed38b349fcfdc7c9eadab
siyaphat <siyaphat@g.lpru.ac.th>
2025-11-21 12:00:11 +0700

    first commit

---
+++ hello_world/android/app/src/main/kotlin/com/siyaphat/hello_world/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.siyaphat.hello_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ hello_world/lib/hello.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  print('hello word');
+  print('My name is \n siyaphat');
+  print('''how ot learn dart faster
+  way in this class''');
+  print(r'this is \n nultiline \n string');
+}
