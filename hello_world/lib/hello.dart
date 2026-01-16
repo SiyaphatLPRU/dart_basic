c81e118708bd5df241d318b31614339acd72d4ba
siyaphat <siyaphat@g.lpru.ac.th>
2026-01-16 11:00:39 +0700

    Add initial macOS support and project configuration
    - Created AppInfo.xcconfig for application-level settings.
    - Added Debug.xcconfig and Release.xcconfig for build configurations.
    - Introduced Warnings.xcconfig to manage compiler warnings.
    - Implemented entitlements for Debug and Release configurations.
    - Created Info.plist for macOS application metadata.
    - Added MainFlutterWindow.swift to manage the main application window.
    - Set up RunnerTests.swift for initial unit testing.
    - Generated pubspec.yaml and pubspec.lock for package management.
    - Added basic widget test in widget_test.dart.
    - Included web assets: favicon, icons, index.html, and manifest.json.

--- hello_world/lib/hello.dart
+++
@@ -1,7 +0,0 @@
-void main(List<String> args) {
-  print('hello word');
-  print('My name is \n siyaphat');
-  print('''how ot learn dart faster
-  way in this class''');
-  print(r'this is \n nultiline \n string');
-}
